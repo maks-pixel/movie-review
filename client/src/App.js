1bd51b9d7e66f328387ca2215df2a1420e387394
Makda Girmay <makdagirmay4@gmail.com>
2023-02-02 13:05:40 -0500

    connecting the front end to the back end using axios using axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
-
+import React, {useState, useEffect} from "react"
 import './App.css';
+import Axios from 'axios'
 
 function App() {
+
+  const [movieName, setMovieName] = useState('')
+  const [review, setReview] = useState('')
+
+  const submitReview = () => {
+    Axios.post()// add the URL for the insert query to connect to the backend in the server/index.js file
+  };
+
   return (
     <div className="App">
       <h1>CRUD APPLICATION</h1>
 
       <div className='form'>
         <label> Movie Name: </label>
-        <input type="text" name="movieName" />
+        <input type="text" name="movieName" onChange={(e) => {
+          setMovieName(e.target.value);
+        }}
+        />
         <label> Review: </label>
-        <input type="text" name='review' />
-        
-        <button> submit </button>
+        <input type="text" name='review' onChange={(e) => {
+          setReview(e.target.value);
+        }}
+        />
+
+        <button onClick = {submitReview}> submit </button>
       </div>
 
     </div>
