08f1b064c9eed2e75646fb93a33e7913829aa1b9
Makda Girmay <makdagirmay4@gmail.com>
2023-02-02 13:08:47 -0500

    was in the wrong file to push server side up. now adding server files. mysql database is set up with functioning insert query

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
-const express = require ('express')
-const app = express()    
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
 
-app.get('/',( req, res)=> {
-    res.send('hello world');
+//make a .env file to put info for database in so it doesnt get posted up
+const db = mysql.createPool({
+//sql database infromtation
+
+});
+
+app.get('/', (req, res) => {
+    // const sqlInsert = "INSERT INTO movie_reviews (movie_name, movie_review) VALUES ('shrek', 'good movie');";
+    // db.query( sqlInsert, (err, result) => {
+    //     res.send('hello world');
+    // })
+    // this is to make sure that the insert into the database works
+    // add the correct get / route that is used in the axios function in the front end and match the conformation being passed through
+
 });
 app.listen(3001, () => {
     console.log("running on port 3001")
