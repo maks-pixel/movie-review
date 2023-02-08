372cb66e8c6fc9d8311e4767edbbe60232454e13
Makda Girmay <makdagirmay4@gmail.com>
2023-02-08 12:25:49 -0500

    adding buttons for delete and update future funtcions, and rendering added reviews without reload. plus some styling for cards

--- client/src/App.js
+++ client/src/App.js
@@ -14,14 +14,15 @@ function App() {
       setMovieList(response.data)
     })
   })
-
+  
   const submitReview = () => {
     Axios.post("http://localhost:3001/api/insert", {movieName: movieName, movieReview: movieReview
-  }).then(() => {
-    alert('successful insert to database')
-  })
-  // add the URL for the insert query to connect to the backend in the server/index.js file
+  });
+  //originally using a .then but that made it so you had to reload the page to have the movie update
+    setMovieList([... movieReviewList, {movieName: movieName, movieReview: movieReview},
+    ]);
   };
+
   //make sure that when mapping the response from the get function connected to the back end you match the value names used in the console.log 
   return (
     <div className="App">
@@ -42,7 +43,13 @@ function App() {
         <button onClick = {submitReview}> submit </button>
         
         {movieReviewList.map((val) => {
-          return<h1>Movie Name: {val.movie_name} | Movie Review: {val.movie_review}</h1>
+          return (
+          <div className="card">
+            <h1>{val.movie_name}</h1>
+            <p> Movie Review: {val.movie_review}</p>
+            <button>delete</button>
+            <button>update</button>
+            </div>)
         })}   
         </div>
     </div>
